0400d279335d26999f8f456930296f9c8d93b026
Дмитрий Василенко <dmitrijvasilenko@MacBook-Air-Dmitrij.local>
2022-05-06 11:38:00 +0300

    Добавляет Header, Footer, api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import UserContext from './contexts/userContext';
+import api from './utils/api';
+import './index.css';
 
 export const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    api.getUser()
+    .then(user => setUser(user))
+    .catch(err => alert(err))
+  }, [])
+
   return (
-    <div>App</div>
+    <UserContext.Provider value={{user, setUser}}>
+      <div className='app'>
+        <Header/>
+        <Footer/>
+      </div>
+    </UserContext.Provider>
   )
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import UserContext from '../../contexts/userContext';
+import { AppBar, Container, Toolbar, Typography, Box, Avatar, Chip } from '@mui/material';
+import { Logo } from '../Logo';
 
 export const Header = () => {
+  const {user} = useContext(UserContext);
+
   return (
-    <div>Header</div>
+    <AppBar position='sticky'>
+      <Container fixed>
+        <Toolbar>
+          <Box component='div' sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            gap: '10px'
+          }}>
+            <Logo/>
+            <Typography>ADM Posts</Typography>
+          </Box>
+          <Box component='div' sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            flexGrow: 1
+          }}>
+            {user && <Chip 
+              avatar={<Avatar alt='avatar' src={user.avatar}/>}
+              label={user.name}
+              clickable={true}
+              sx={{marginRight: 4}}
+            />}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   )
 }
